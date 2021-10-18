ca10faa35d1d32b1ee640c271adbbd755fe3b416
leticsousa <leticsousa@outlook.com>
2021-10-17 22:06:23 -0300

    Exercícios 14 e 15, seção 4

---
+++ exer_sec_04_15.py
@@ -0,0 +1,5 @@
+ang_radianos = float(input("Digite o ângulo em radianos:"))
+
+ang_graus = (ang_radianos * 180)/3.14
+
+print(f"O ângulo em graus é {round(ang_graus, 2)}")
---
+++ exerc_sec_04_14.py
@@ -0,0 +1,5 @@
+ang_graus = float(input("Digite o ângulo em graus:"))
+
+ang_rad = (ang_graus * 3.14)/180
+
+print(f"O ângulo em radianos é de {round(ang_rad, 2)} graus")
